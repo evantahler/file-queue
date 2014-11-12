bebbc42cbde751ad876ac2ce9429b4da89df29a4
Evan Tahler <evantahler@gmail.com>
2014-11-11 23:45:08 -0800

    basics

---
+++ index.js
@@ -0,0 +1,3 @@
+exports.queue     = require(__dirname + "/lib/queue.js").queue;
+exports.worker    = require(__dirname + "/lib/worker.js").worker;
+exports.scheduler = require(__dirname + "/lib/scheduler.js").scheduler;
