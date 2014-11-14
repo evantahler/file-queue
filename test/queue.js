5c26a9b1ad6f8dc6dcdce964cbcb85d51a4ea0fb
Evan Tahler <evan@taskrabbit.com>
2014-11-14 11:26:52 -0800

    more tests

--- test/queue.js
+++ test/queue.js
@@ -6,6 +6,7 @@ var testDir = '/tmp/test';
 
 var FileQueue = require(__dirname + path.sep + '..' + path.sep + 'index.js');
 var queue = new FileQueue.queue(testDir);
+var originalGetTime = Date.prototype.getTime;
 
 var cleanup = function(callback){
   try{
@@ -19,8 +20,15 @@ var cleanup = function(callback){
 
 describe('queue', function(){
 
+  afterEach(function(){
+    Date.prototype.getTime = originalGetTime;
+  });
+
   beforeEach(function(done){
     cleanup(done);
+    // hack to lock in time; to account for time drift in tests
+    var now = new Date().getTime();
+    Date.prototype.getTime = function(){ return now; };
   });
 
   it('should save the default path', function(){
@@ -52,9 +60,7 @@ describe('queue', function(){
     var funcs = [];
     while(i < 100){
       funcs.push(function(callback){
-        queue.enqueue('test_queue', 'doStuffLater', {a: 1, b: 2}, function(){
-          callback();
-        });
+        queue.enqueue('test_queue', 'doStuffLater', {a: 1, b: 2}, function(){ callback(); });
       });
       i++;
     }
@@ -67,11 +73,106 @@ describe('queue', function(){
     });
   });
 
-  it('can save a job for later (enqueueAt)');
-  it('can save a job for later (enqueueIn)');
-  it('can inspect queues');
-  it('can inspect timestamps');
-  it('can inspect queue length');
+  it('can save a job for later (enqueueAt)', function(done){
+    var now = new Date().getTime();
+    queue.enqueueAt((now + 100), 'test_queue', 'doStuffLater', {a: 1, b: 2}, function(err, filename, payload){
+      should.not.exist(err);
+      
+      var payloadObj = JSON.parse(payload);
+      payloadObj.class.should.equal('doStuffLater');
+      payloadObj.queue.should.equal('test_queue');
+      payloadObj.args[0].a.should.equal(1);
+      payloadObj.args[0].b.should.equal(2);
+
+      var bodyPayload;
+      bodyPayload = JSON.parse(fs.readFileSync(testDir + path.sep + 'delayed' + path.sep + Math.floor(now/1000) + path.sep + filename));
+      bodyPayload.class.should.equal('doStuffLater');
+      bodyPayload.queue.should.equal('test_queue');
+      bodyPayload.args[0].a.should.equal(1);
+      bodyPayload.args[0].b.should.equal(2);
+
+      done();
+    });
+  });
+
+  it('can save a job for later (enqueueIn)', function(done){
+    var now = new Date().getTime();
+    queue.enqueueIn(100, 'test_queue', 'doStuffLater', {a: 1, b: 2}, function(err, filename, payload){
+      should.not.exist(err);
+      
+      var payloadObj = JSON.parse(payload);
+      payloadObj.class.should.equal('doStuffLater');
+      payloadObj.queue.should.equal('test_queue');
+      payloadObj.args[0].a.should.equal(1);
+      payloadObj.args[0].b.should.equal(2);
+
+      var bodyPayload;
+      try{
+        bodyPayload = JSON.parse(fs.readFileSync(testDir + path.sep + 'delayed' + path.sep + Math.floor(now/1000) + path.sep + filename));
+      }catch(e){
+        if(e.code !== 'ENOENT'){ throw e; }
+        // add a second
+        bodyPayload = JSON.parse(fs.readFileSync(testDir + path.sep + 'delayed' + path.sep + (Math.floor(now/1000) + 1) + path.sep + filename));
+      }
+      bodyPayload.class.should.equal('doStuffLater');
+      bodyPayload.queue.should.equal('test_queue');
+      bodyPayload.args[0].a.should.equal(1);
+      bodyPayload.args[0].b.should.equal(2);
+
+      done();
+    });
+  });
+
+  it('can inspect queues', function(done){
+    async.parallel([
+      function(callback){ queue.enqueue('test_queue_a', 'doStuffLater', {}, callback); },
+      function(callback){ queue.enqueue('test_queue_b', 'doStuffLater', {}, callback); },
+      function(callback){ queue.enqueue('test_queue_c', 'doStuffLater', {}, callback); },
+    ], function(err){
+      should.not.exist(err);
+      queue.queues(function(err, queues){
+        should.not.exist(err);
+        queues.length.should.equal(3);
+        queues.indexOf('test_queue_a').should.equal(0);
+        queues.indexOf('test_queue_b').should.equal(1);
+        queues.indexOf('test_queue_c').should.equal(2);
+        done();
+      });
+    });
+  });
+
+  it('can inspect timestamps', function(done){
+    var now = new Date().getTime();
+    async.parallel([
+      function(callback){ queue.enqueueAt(now + 1000, 'test_queue_a', 'doStuffLater', {}, callback); },
+      function(callback){ queue.enqueueAt(now + 2000, 'test_queue_b', 'doStuffLater', {}, callback); },
+      function(callback){ queue.enqueueAt(now + 3000, 'test_queue_c', 'doStuffLater', {}, callback); },
+    ], function(err){
+      queue.timestamps(function(err, timestamps){
+        should.not.exist(err);
+        timestamps.indexOf(Math.floor(now/1000) + 1).should.equal(0);
+        timestamps.indexOf(Math.floor(now/1000) + 2).should.equal(1);
+        timestamps.indexOf(Math.floor(now/1000) + 3).should.equal(2);
+        done();
+      });
+    });
+  });
+
+  it('can inspect queue length', function(done){
+    async.parallel([
+      function(callback){ queue.enqueue('test_queue_a', 'doStuffLater', {}, callback); },
+      function(callback){ queue.enqueue('test_queue_a', 'doStuffLater', {}, callback); },
+      function(callback){ queue.enqueue('test_queue_a', 'doStuffLater', {}, callback); },
+    ], function(err){
+      should.not.exist(err);
+      queue.length('test_queue_a', function(err, length){
+        should.not.exist(err);
+        length.should.equal(3);
+        done();
+      });
+    });
+  });
+
   it('can inspect timestamp length');
   it('can delte a queue');
   it('can delte an item from a queue');
